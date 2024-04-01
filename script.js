0847eefa1b9cbeeb2d9df1f9fdf8f47e20cbecef
Yuvaraj M <85824565+yuvaraj75@users.noreply.github.com>
2024-04-01 17:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ root@yuvxraj_Hub:~/Portfolio# exit
 
 let i = 0;
 const terminal = document.getElementById('terminal');
-const speed = 50; // Typing speed
+const speed = 60; // Typing speed
 
 function typeOut() {
     if (i < terminalText.length) {
